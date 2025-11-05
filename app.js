45b174e2fac431597abc6353b8427c15e1998942
hrishikeshkhandagale <hrishikeshkhandagale2@gmail.com>
2025-11-05 17:06:27 +0530

    added

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Nadd karti kai zala kii...');
+  res.send('Nadd karti kai zala ');
 });
 
 app.listen(port, () => {
